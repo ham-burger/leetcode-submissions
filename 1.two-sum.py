ad3b64fb027c85637323a792182c418929f6d314
kotaro.kudo <devkudou@gmail.com>
2022-01-04 22:16:24 +0900

    accepted [1]

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -7,16 +7,18 @@
 # @lc code=start
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        set_nums = set(nums)
         # numsの要素を読み込む
         for i, num in enumerate(nums):
             # 得られた要素とtargetから足りないもう一辺を算出する
             expected = target - num
+
+            # 現在のindex以降でsetを作成する
+            set_nums = set(nums[i+1:])
+
+            # listの中に解となる値が存在すればindexを返却,無ければ継続する
             is_exist = expected in set_nums
-            # listの中にそれが存在すれば、return,無ければ継続する
             if is_exist:
-                # 存在した場合はindexを返却する
-                return [i, nums.index(expected)]
+                return [i, nums.index(expected, i+1)]
             else:
                 continue
 
