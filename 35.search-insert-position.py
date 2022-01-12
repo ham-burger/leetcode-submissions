121d5d383f4978357aa34e2e5355da76a33189ac
kotaro.kudo <devkudou@gmail.com>
2022-01-12 09:56:17 +0900

    refactor [35]

--- 35.search-insert-position.py
+++ 35.search-insert-position.py
@@ -6,45 +6,6 @@
 
 # @lc code=start
 class Solution:
-    """
-    (1)
-    input
-    [1,3,5,6]
-    5
-
-    process
-    
-    3
-    5
-    
-    output
-    0
-
-    (2)
-    input
-    [1,3,5,6]
-    2
-
-    process
-    3
-    1
-
-    output
-    1
-
-    (4)
-    input
-    [1,3,5,6,8]
-    4
-
-    process
-    5
-    3
-
-    output
-    2
-
-    """
     def searchInsert(self, nums: List[int], target: int) -> int:
         start = 0
         last = len(nums) - 1
@@ -57,25 +18,16 @@ class Solution:
                 return last
             elif(nums[last] < target):
                 return last + 1
-
-            # 範囲内なら範囲を絞り込んでいく
-            diff = last - start
-            index = start + (diff // 2)
-            harf = nums[index]
-
-            # 丁度範囲の境目の場合は確定
-            if(harf == target):
-                return index
-            elif(harf < target):
-                start = index + 1
             else:
-                last = index - 1
+                # 範囲内なら範囲を絞り込んでいく
+                diff = last - start
+                index = start + (diff // 2)
+                harf = nums[index]
 
-        # # todo: 終了条件変更する
-        # if(nums[start] < target):
-        #     return last + 1
-        # else:
-        #     return start
-#
+                # 丁度範囲の境目の場合は確定
+                if(harf < target):
+                    start = index + 1
+                else:
+                    last = index - 1
 # @lc code=end
 
