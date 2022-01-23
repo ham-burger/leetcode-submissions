81397255dd6455cb5c4306be872b9f8cbcd0cbc7
kotaro.kudo <devkudou@gmail.com>
2022-01-23 10:59:03 +0900

    [35] add code that was mentioned in the discussion

--- 35.search-insert-position.py
+++ 35.search-insert-position.py
@@ -10,24 +10,47 @@ class Solution:
         start = 0
         last = len(nums) - 1
 
-        while(True):
-            # 範囲外なら確定
-            if(nums[start] >= target):
-                return start
-            elif(nums[last] == target):
-                return last
-            elif(nums[last] < target):
-                return last + 1
-            else:
-                # 範囲内なら範囲を絞り込んでいく
-                diff = last - start
-                index = start + (diff // 2)
-                harf = nums[index]
+        # while(True):
+        #     # 範囲外なら確定
+        #     if(nums[start] >= target):
+        #         return start
+        #     elif(nums[last] == target):
+        #         return last
+        #     elif(nums[last] < target):
+        #         return last +1
+        #     else:
+        #         # 範囲内なら範囲を絞り込んでいく
+        #         diff = last - start
+        #         index = start + (diff // 2)
+        #         harf = nums[index]
 
-                # 丁度範囲の境目の場合は確定
-                if(harf < target):
-                    start = index + 1
-                else:
-                    last = index - 1
+        #         # 丁度範囲の境目の場合は確定
+        #         if(harf < target):
+        #             start = index + 1
+        #         else:
+        #             last = index - 1
+        # while(start < last):
+        #     # diff = last - start
+        #     # middle = start + (diff // 2)
+        #     middle = (start + last) // 2
+        #     if(nums[middle] == target):
+        #         return middle
+        #     elif(middle < target):
+        #         start = middle + 1
+        #     else:
+        #         last = middle - 1
+
+        # return start
+
+        low, high = 0, len(nums)
+        while low < high:
+            mid = (low + high) // 2
+            if target > nums[mid]:
+                low = mid + 1
+            else:
+                high = mid
+        return low
+
+
 # @lc code=end
 
